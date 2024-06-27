5ad2e983c3f49c45aeb6e3fa05e242a7b157d1a8
Max Rossmannek <oss@zurich.ibm.com>
2024-06-27 08:43:05 +0200

    docs: clean up of ClassicalShadows

--- povm_toolbox/library/classical_shadows.py
+++ povm_toolbox/library/classical_shadows.py
@@ -8,7 +8,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""TODO."""
+"""ClassicalShadows."""
 
 from __future__ import annotations
 
@@ -19,43 +19,56 @@ from .locally_biased_classical_shadows import LocallyBiasedClassicalShadows
 
 
 class ClassicalShadows(LocallyBiasedClassicalShadows):
-    """A classical shadows POVM."""
+    """A classical shadows POVM.
+
+    This is a special case of the :class:`LocallyBiasedClassicalShadows`, where the bias is taken to
+    be uniform. That is, there is an equal probability to perform a measurement in the Z, X and Y
+    bases.
+
+    The example below shows how you construct a classical shadows POVM. It plots a visual
+    representation of the POVM's definition to exemplify the equal measurement probabilities.
+
+    .. plot::
+       :context:
+       :include-source:
+
+       >>> from povm_toolbox.library import ClassicalShadows
+       >>> povm = ClassicalShadows(2, measurement_twirl=True, shot_repetitions=10)
+       >>> print(povm)
+       ClassicalShadows(num_qubits=2)
+       >>> povm.definition().draw_bloch()
+       <Figure size 1000x500 with 2 Axes>
+    """
 
     def __init__(
         self,
         num_qubits: int,
-        measurement_twirl: bool = False,
+        *,
         measurement_layout: list[int] | None = None,  # TODO: add | Layout
+        measurement_twirl: bool = False,
         shot_repetitions: int = 1,
         seed_rng: int | Generator | None = None,
-    ):
-        """Implement a classical shadows POVM.
-
-        This is a special case of a :class:`LocallyBiasedClassicalShadows`, where
-        the bias are taken to be uniform. That is, there is an equal probability
-        to perform a measurement in the Z, X and Y bases.
+    ) -> None:
+        """Initialize a classical shadows POVM.
 
         Args:
             num_qubits: the number of qubits.
-            measurement_twirl : option to randomly twirl the measurements. For each single-qubit
+            measurement_layout: optional list of indices specifying on which qubits the POVM acts.
+                See :attr:`.measurement_layout` for more details.
+            measurement_twirl: whether to randomly twirl the measurements. For each single-qubit
                 projective measurement, random twirling is equivalent to randomly flipping the
-                measurement. This is equivalent to randomly taking the opposite Bloch vector in
-                the Bloch sphere representation.
-            measurement_layout: list of indices specifying on which qubits the POVM
-                acts. If None, two cases can be distinguished: 1) if a circuit supplied
-                to the :meth:`.compose_circuits` has been transpiled, its final
-                transpile layout will be used as default value, 2) otherwise, a
-                simple one-to-one layout ``list(range(num_qubits))`` is used.
-            shot_repetitions: number of times the measurement is repeated for each
-                sampled PVM. More precisely, a new PVM is sampled for all ``shots``
-                (i.e. the number of times as specified by the user via the ``shots``
-                argument of the method :meth:`.POVMSampler.run`). Then, the parameter
-                ``shot_repetitions`` states how many times we repeat the measurement
-                for each sampled PVM (default is 1). Therefore, the effective total
-                number of measurement shots is ``shots`` multiplied by ``shot_repetitions``.
-            seed_rng: optional seed to fix the :class:`numpy.random.Generator` used to
-                sample measurement bases. The user can also directly provide a random
-                generator. If None, a random seed will be used.
+                measurement. This is equivalent to randomly taking the opposite Bloch vector in the
+                Bloch sphere representation.
+            shot_repetitions: number of times the measurement is repeated for each sampled PVM. More
+                precisely, a new PVM is sampled for all ``shots`` (i.e. the number of times as
+                specified by the user via the ``shots`` argument of the method
+                :meth:`.POVMSampler.run`). Then, the parameter ``shot_repetitions`` states how many
+                times we repeat the measurement for each sampled PVM (default is 1). Therefore, the
+                effective total number of measurement shots is ``shots`` multiplied by
+                ``shot_repetitions``.
+            seed_rng: optional seed to fix the :class:`numpy.random.Generator` used to sample
+                measurement bases. The user can also directly provide a random generator.
+                If ``None``, a random seed will be used.
         """
         bias = 1.0 / 3.0 * np.ones(3)
         super().__init__(
