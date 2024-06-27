76b7c20edbe00e5c4002184361985c163e903828
Timothée Dao <Timothee.Dao1@ibm.com>
2024-06-27 13:32:03 +0200

    Add TODOs

---
+++ test/sampler/test_povm_sampler_pub.py
@@ -0,0 +1,56 @@
+# (C) Copyright IBM 2024.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Tests for the POVMSamplerPub class."""
+
+from unittest import TestCase
+
+
+class TestPOVMSamplerPub(TestCase):
+    """Tests for the ``POVMSamplerPub`` class."""
+
+    def test_initialization(self):
+        """Test that the ``__init__`` method works correctly."""
+        with self.subTest("Initialization with validation."):
+            # TODO
+            pass
+        with self.subTest("Initialization without validation."):
+            # TODO
+            pass
+
+    def test_properties(self):
+        """Test that the public attributes work correctly."""
+        with self.subTest("Test ``circuit`` attribute."):
+            # TODO
+            pass
+        with self.subTest("Test ``parameter_values`` attribute."):
+            # TODO
+            pass
+        with self.subTest("Test ``shots`` attribute."):
+            # TODO
+            pass
+        with self.subTest("Test ``povm`` attribute."):
+            # TODO
+            pass
+
+    def test_coerce(self):
+        """Test that the ``coerce`` method works correctly."""
+        # TODO
+        return
+
+    def test_validate(self):
+        """Test that the ``validate`` method works correctly."""
+        # TODO
+        return
+
+    def test_to_sampler_pub(self):
+        """Test that the ``to_sampler_pub`` method works correctly."""
+        # TODO
+        return
