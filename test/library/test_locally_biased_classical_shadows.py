a9614938114673942df9764af6296a27205aec9e
timmintam <100034085+timmintam@users.noreply.github.com>
2024-07-03 10:01:34 +0200

    Complete the unittests for the `LocallyBiasedClassicalShadows` class (#53)
    * Complete tests
    
    * Typo
    
    * typo 2

--- test/library/test_locally_biased_classical_shadows.py
+++ test/library/test_locally_biased_classical_shadows.py
@@ -14,13 +14,19 @@ from unittest import TestCase
 
 import numpy as np
 from povm_toolbox.library import LocallyBiasedClassicalShadows
+from povm_toolbox.post_processor import POVMPostProcessor
 from povm_toolbox.quantum_info.single_qubit_povm import SingleQubitPOVM
-from qiskit.quantum_info import Operator
+from povm_toolbox.sampler import POVMSampler
+from qiskit import QuantumCircuit
+from qiskit.primitives import StatevectorSampler
+from qiskit.quantum_info import Operator, SparsePauliOp
 
 
 class TestRandomizedPMs(TestCase):
-    def __init__(self, methodName: str = "runTest") -> None:
-        super().__init__(methodName)
+    RNG_SEED = 13
+
+    def setUp(self) -> None:
+        super().setUp()
 
         basis_0 = np.array([1.0, 0], dtype=complex)
         basis_1 = np.array([0, 1.0], dtype=complex)
@@ -35,6 +41,60 @@ class TestRandomizedPMs(TestCase):
         self.X1 = np.outer(basis_minus, basis_minus.conj())
         self.Y0 = np.outer(basis_plus_i, basis_plus_i.conj())
         self.Y1 = np.outer(basis_minus_i, basis_minus_i.conj())
+
+    def test_init(self):
+        """Test the implementation of locally-biased classical shadows."""
+
+        qc = QuantumCircuit(2)
+        qc.h(0)
+
+        num_qubits = qc.num_qubits
+
+        with self.subTest("Test uniform bias across qubits."):
+            measurement = LocallyBiasedClassicalShadows(
+                num_qubits,
+                bias=np.array([0.2, 0.3, 0.5]),
+                seed_rng=self.RNG_SEED,
+            )
+            sampler = StatevectorSampler(seed=self.RNG_SEED)
+            povm_sampler = POVMSampler(sampler=sampler)
+
+            job = povm_sampler.run([qc], shots=32, povm=measurement)
+            pub_result = job.result()[0]
+
+            post_processor = POVMPostProcessor(pub_result)
+
+            observable = SparsePauliOp(["ZI"], coeffs=[1.0])
+            exp_value, std = post_processor.get_expectation_value(observable)
+            self.assertAlmostEqual(exp_value, 0.9374999999999998)
+            self.assertAlmostEqual(std, 0.3505108598934214)
+            observable = SparsePauliOp(["ZY"], coeffs=[1.0])
+            exp_value, std = post_processor.get_expectation_value(observable)
+            self.assertAlmostEqual(exp_value, -1.2499999999999993)
+            self.assertAlmostEqual(std, 0.593988704139364)
+
+        with self.subTest("Test specific bias for each qubit."):
+            measurement = LocallyBiasedClassicalShadows(
+                num_qubits,
+                bias=np.array([[0.5, 0.1, 0.4], [0.3, 0.4, 0.3]]),
+                seed_rng=self.RNG_SEED,
+            )
+            sampler = StatevectorSampler(seed=self.RNG_SEED)
+            povm_sampler = POVMSampler(sampler=sampler)
+
+            job = povm_sampler.run([qc], shots=32, povm=measurement)
+            pub_result = job.result()[0]
+
+            post_processor = POVMPostProcessor(pub_result)
+
+            observable = SparsePauliOp(["ZI"], coeffs=[1.0])
+            exp_value, std = post_processor.get_expectation_value(observable)
+            self.assertAlmostEqual(exp_value, 0.7291666666666665)
+            self.assertAlmostEqual(std, 0.24749529339140175)
+            observable = SparsePauliOp(["ZY"], coeffs=[1.0])
+            exp_value, std = post_processor.get_expectation_value(observable)
+            self.assertAlmostEqual(exp_value, -0.78125)
+            self.assertAlmostEqual(std, 0.43626216977818505)
 
     def test_qc_build(self):
         """Test if we can build a LB Classical Shadow POVM from the generic class"""
@@ -60,3 +120,12 @@ class TestRandomizedPMs(TestCase):
                 self.assertEqual(cs_povm._frames[(i,)].num_outcomes, sqpovm.num_outcomes)
                 for k in range(sqpovm.num_outcomes):
                     self.assertTrue(np.allclose(cs_povm._frames[(i,)][k], sqpovm[k]))
+
+    def test_repr(self):
+        """Test that the ``__repr__`` method works correctly."""
+        lbcs_str = "LocallyBiasedClassicalShadows(num_qubits=1, bias=array([[0.2, 0.3, 0.5]]))"
+        povm = LocallyBiasedClassicalShadows(
+            1,
+            bias=np.array([0.2, 0.3, 0.5]),
+        )
+        self.assertEqual(povm.__repr__(), lbcs_str)
