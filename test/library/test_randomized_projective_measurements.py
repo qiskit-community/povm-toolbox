8257fc1ee0c5a9880c50d62911208ef0bf359aac
Timothée Dao <Timothee.Dao1@ibm.com>
2024-07-03 14:03:45 +0200

    Add tests and TODOs

--- test/library/test_randomized_projective_measurements.py
+++ test/library/test_randomized_projective_measurements.py
@@ -13,7 +13,6 @@
 from unittest import TestCase
 
 import numpy as np
-from numpy.random import default_rng
 from povm_toolbox.library import ClassicalShadows, RandomizedProjectiveMeasurements
 from povm_toolbox.post_processor import POVMPostProcessor
 from povm_toolbox.sampler import POVMSampler
@@ -23,8 +22,10 @@ from qiskit.quantum_info import SparsePauliOp
 
 
 class TestRandomizedPMs(TestCase):
-    def __init__(self, methodName: str = "runTest") -> None:
-        super().__init__(methodName)
+    RNG_SEED = 239486
+
+    def setUp(self) -> None:
+        super().setUp()
 
         basis_0 = np.array([1.0, 0], dtype=complex)
         basis_1 = np.array([0, 1.0], dtype=complex)
@@ -40,9 +41,60 @@ class TestRandomizedPMs(TestCase):
         self.Y0 = np.outer(basis_plus_i, basis_plus_i.conj())
         self.Y1 = np.outer(basis_minus_i, basis_minus_i.conj())
 
+    def test_init_errors(self):
+        """Test that the ``__init__`` method raises errors correctly."""
+        # Sanity check
+        measurement = RandomizedProjectiveMeasurements(
+            1, bias=np.array([0.5, 0.5]), angles=np.array([0.0, 0.0, 0.5, 0.0])
+        )
+        self.assertIsInstance(measurement, RandomizedProjectiveMeasurements)
+        with self.subTest("Incompatible ``bias`` and ``angles`` shapes.") and self.assertRaises(
+            ValueError
+        ):
+            RandomizedProjectiveMeasurements(
+                1, bias=np.array([0.5, 0.5]), angles=np.array([0.0, 0.0, 0.5, 0.0, 0.4])
+            )
+        with self.subTest(
+            "Shape of ``bias`` incompatible with number of qubits."
+        ) and self.assertRaises(ValueError):
+            RandomizedProjectiveMeasurements(
+                1, bias=np.array([[0.5, 0.5], [0.5, 0.5]]), angles=np.array([0.0, 0.0, 0.5, 0.0])
+            )
+        with self.subTest("Too many dims in ``bias``.") and self.assertRaises(ValueError):
+            RandomizedProjectiveMeasurements(
+                1, bias=np.array([[[0.5, 0.5]]]), angles=np.array([0.0, 0.0, 0.5, 0.0])
+            )
+        with self.subTest("Negative value in ``bias``.") and self.assertRaises(ValueError):
+            RandomizedProjectiveMeasurements(
+                1, bias=np.array([1.5, -0.5]), angles=np.array([0.0, 0.0, 0.5, 0.0])
+            )
+        with self.subTest("``bias`` not summing up to one.") and self.assertRaises(ValueError):
+            RandomizedProjectiveMeasurements(
+                1, bias=np.array([0.5, 0.4]), angles=np.array([0.0, 0.0, 0.5, 0.0])
+            )
+        with self.subTest("``bias`` not summing up to one.") and self.assertRaises(ValueError):
+            RandomizedProjectiveMeasurements(
+                1, bias=np.array([[0.5, 0.4], [0.5, 0.6]]), angles=np.array([0.0, 0.0, 0.5, 0.0])
+            )
+        with self.subTest(
+            "Shape of ``angles`` incompatible with number of qubits."
+        ) and self.assertRaises(ValueError):
+            RandomizedProjectiveMeasurements(
+                1,
+                bias=np.array([0.5, 0.5]),
+                angles=np.array([[0.0, 0.0, 0.5, 0.0], [0.0, 0.0, 0.5, 0.0]]),
+            )
+        with self.subTest("Too many dims in ``angles``.") and self.assertRaises(ValueError):
+            RandomizedProjectiveMeasurements(
+                1, bias=np.array([0.5, 0.5]), angles=np.array([[[0.0, 0.0, 0.5, 0.0]]])
+            )
+        with self.subTest("Invalid type for ``seed_rng``.") and self.assertRaises(TypeError):
+            RandomizedProjectiveMeasurements(
+                1, bias=np.array([0.5, 0.5]), angles=np.array([0.0, 0.0, 0.5, 0.0]), seed_rng=1.2
+            )
+
     def test_qc_build(self):
         """Test if we can build a QuantumCircuit."""
-
         for num_qubits in range(1, 11):
             q = np.random.uniform(0, 5, size=3 * num_qubits).reshape((num_qubits, 3))
             q /= q.sum(axis=1)[:, np.newaxis]
@@ -57,20 +109,28 @@ class TestRandomizedPMs(TestCase):
 
             self.assertEqual(qc.num_qubits, num_qubits)
 
+    def test_to_sampler_pub(self):
+        """Test that the ``to_sampler_pub`` method works correctly."""
+        _ = RandomizedProjectiveMeasurements(
+            1,
+            bias=np.array([0.3, 0.7]),
+            angles=np.array([0.0, 0.0, 0.5, 0.0]),
+            seed_rng=self.RNG_SEED,
+        )
+        qc = QuantumCircuit(2)
+        qc.h(0)
+
+        # TODO
+
     def test_twirling(self):
         """Test if the twirling option works correctly."""
-        rng = default_rng(13)
-
         qc = QuantumCircuit(2)
         qc.h(0)
 
         num_qubits = qc.num_qubits
-        measurement = ClassicalShadows(num_qubits, seed_rng=rng, measurement_twirl=True)
+        measurement = ClassicalShadows(num_qubits, seed_rng=self.RNG_SEED, measurement_twirl=True)
 
-        rng2 = default_rng(26)
-
-        sampler = StatevectorSampler(seed=rng2)
-        povm_sampler = POVMSampler(sampler=sampler)
+        povm_sampler = POVMSampler(sampler=StatevectorSampler(seed=self.RNG_SEED))
 
         job = povm_sampler.run([qc], shots=128, povm=measurement)
         pub_result = job.result()[0]
@@ -79,16 +139,16 @@ class TestRandomizedPMs(TestCase):
 
         observable = SparsePauliOp(["ZI"], coeffs=[1.0])
         exp_value, std = post_processor.get_expectation_value(observable)
-        self.assertAlmostEqual(exp_value, 0.9843749999999998)
-        self.assertAlmostEqual(std, 0.12499231029496978)
+        self.assertAlmostEqual(exp_value, 1.0078125000000002)
+        self.assertAlmostEqual(std, 0.1257341109337995)
         observable = SparsePauliOp(["IZ"], coeffs=[1.0])
         exp_value, std = post_processor.get_expectation_value(observable)
-        self.assertAlmostEqual(exp_value, 0.07031249999999983)
-        self.assertAlmostEqual(std, 0.13085857162022752)
+        self.assertAlmostEqual(exp_value, 0.32812500000000006)
+        self.assertAlmostEqual(std, 0.15333777198692233)
         observable = SparsePauliOp(["ZY"], coeffs=[1.0])
         exp_value, std = post_processor.get_expectation_value(observable)
-        self.assertAlmostEqual(exp_value, 0.0)
-        self.assertAlmostEqual(std, 0.26411902291905537)
+        self.assertAlmostEqual(exp_value, -0.42187500000000006)
+        self.assertAlmostEqual(std, 0.24164416287543897)
         observable = SparsePauliOp(["IX"], coeffs=[1.0])
         exp_value, std = post_processor.get_expectation_value(observable)
         self.assertAlmostEqual(exp_value, 1.1718749999999998)
@@ -96,18 +156,13 @@ class TestRandomizedPMs(TestCase):
 
     def test_shot_repetitions(self):
         """Test if the twirling option works correctly."""
-        rng = default_rng(65)
-
         qc = QuantumCircuit(2)
         qc.h(0)
 
         num_qubits = qc.num_qubits
-        measurement = ClassicalShadows(num_qubits, seed_rng=rng, shot_repetitions=7)
+        measurement = ClassicalShadows(num_qubits, seed_rng=self.RNG_SEED, shot_repetitions=7)
 
-        rng2 = default_rng(56)
-
-        sampler = StatevectorSampler(seed=rng2)
-        povm_sampler = POVMSampler(sampler=sampler)
+        povm_sampler = POVMSampler(sampler=StatevectorSampler(seed=self.RNG_SEED))
 
         job = povm_sampler.run([qc], shots=128, povm=measurement)
         pub_result = job.result()[0]
@@ -119,10 +174,26 @@ class TestRandomizedPMs(TestCase):
 
         observable = SparsePauliOp(["ZI"], coeffs=[1.0])
         exp_value, std = post_processor.get_expectation_value(observable)
-        self.assertAlmostEqual(exp_value, 1.0312499999999998)
-        self.assertAlmostEqual(std, 0.04762837328043843)
+        self.assertAlmostEqual(exp_value, 0.9843750000000002)
+        self.assertAlmostEqual(std, 0.04708403113719653)
 
     # TODO: write a unittest for each public method of RandomizedProjectiveMeasurements
 
     # TODO: write a unittest to assert the correct handling of invalid inputs (i.e. verify that
     # errors are raised properly)
+
+    def test_error_private_methods(self):
+        """Test that errors in private methods are raised correctly."""
+        measurement = RandomizedProjectiveMeasurements(
+            2,
+            bias=np.array([0.3, 0.4, 0.3]),
+            angles=np.array([0.0, 0.0, 0.5, 0.0, 0.5, 0.5]),
+            seed_rng=self.RNG_SEED,
+        )
+        qc = QuantumCircuit(2)
+        qc.h(0)
+        povm_sampler = POVMSampler(sampler=StatevectorSampler(seed=self.RNG_SEED))
+        job = povm_sampler.run([qc], shots=128, povm=measurement)
+        _ = job.result()[0]
+
+        # TODO
