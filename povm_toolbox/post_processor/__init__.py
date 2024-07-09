fec7bbb2b97641c0e4a35674038b00a4c2b302ad
Max Rossmannek <oss@zurich.ibm.com>
2024-07-09 11:20:13 +0200

    docs: update the module level `povm_toolbox.post_processor` page

--- povm_toolbox/post_processor/__init__.py
+++ povm_toolbox/post_processor/__init__.py
@@ -8,15 +8,36 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""A module of POVM result post-processing tools.
+"""A module of post-processing tools for POVM results.
 
 .. currentmodule:: povm_toolbox.post_processor
+
+Post-processing of the sampled POVM results (see also the :mod:`~povm_toolbox.sampler` module) is
+required for the computation of expectation values of the observables of interest. In particular,
+one can optimize the Dual frame of a POVM to improve the obtained expectation values.
+For more details, refer to `this how-to guide <../how_tos/dual_optimizer.ipynb>`_.
+
+The PostProcessor
+-----------------
+
+The main entry-point to the post-processing of POVM results is provided by the
+:class:`.POVMPostProcessor` class.
 
 .. autosummary::
    :toctree: ../stubs/
    :nosignatures:
 
    POVMPostProcessor
+
+Various Dual Frames
+-------------------
+
+Additionally, this module provides a number of functions to easily construct specific Dual frames:
+
+.. autosummary::
+   :toctree: ../stubs/
+   :nosignatures:
+
    dual_from_state
    dual_from_marginal_probabilities
    dual_from_empirical_frequencies
