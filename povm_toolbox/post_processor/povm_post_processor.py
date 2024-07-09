ec73c155fef40525b3fe93104afef3a19b3b06e8
Max Rossmannek <oss@zurich.ibm.com>
2024-07-09 13:18:48 +0200

    [docs] Cleanup of the `post_processor` documentation (#61)
    * docs: update the module level `povm_toolbox.post_processor` page
    
    * docs: updates to `POVMPostProcessor`
    
    * docs: updates to `dual_from_state`
    
    * docs: updates to `dual_from_marginal_probabilities`
    
    * docs: updates to `dual_from_empirical_frequencies`
    
    * fix: explicitly convert numpy.float64 to builtin float

--- povm_toolbox/post_processor/povm_post_processor.py
+++ povm_toolbox/post_processor/povm_post_processor.py
@@ -8,11 +8,11 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""TODO."""
+"""POVMPostProcessor."""
 
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, cast
 
 import numpy as np
 from qiskit.quantum_info import SparsePauliOp
@@ -22,7 +22,34 @@ from povm_toolbox.sampler import POVMPubResult
 
 
 class POVMPostProcessor:
-    """A common POVM result post-processor."""
+    """The canonical POVM result post-processor.
+
+    This post-processor implementation provides a straight-forward interface for computing the
+    expectation values (and standard deviations) of any Pauli-based observable. It is initialized
+    with a :class:`.POVMPubResult` as shown below:
+
+    >>> from povm_toolbox.library import ClassicalShadows
+    >>> from povm_toolbox.sampler import POVMSampler
+    >>> from povm_toolbox.post_processor import POVMPostProcessor
+    >>> from qiskit.circuit import QuantumCircuit
+    >>> from qiskit.primitives import StatevectorSampler
+    >>> from qiskit.quantum_info import SparsePauliOp
+    >>> circ = QuantumCircuit(2)
+    >>> _ = circ.h(0)
+    >>> _ = circ.cx(0, 1)
+    >>> povm = ClassicalShadows(2, seed=42)
+    >>> sampler = StatevectorSampler(seed=42)
+    >>> povm_sampler = POVMSampler(sampler)
+    >>> job = povm_sampler.run([circ], povm=povm, shots=16)
+    >>> result = job.result()
+    >>> post_processor = POVMPostProcessor(result[0])
+    >>> post_processor.get_expectation_value(SparsePauliOp("ZI"))  # doctest: +FLOAT_CMP
+    (-0.75, 0.33541019662496846)
+
+    Additionally, this post-processor also supports the customization of the Dual frame in which the
+    decomposition weights of the provided observable are obtained. Check out
+    `this how-to guide <../how_tos/dual_optimizer.ipynb>`_ for more details on how to do this.
+    """
 
     def __init__(
         self,
@@ -33,18 +60,19 @@ class POVMPostProcessor:
 
         Args:
             povm_sample: a result from a POVM sampler run.
-            dual: the subclass of :class:`.BaseDual` that will be used to
-                build the dual frame to the POVM of ``povm_sample``. The dual
-                frame is then used to compute the decomposition weights of any
-                observable.
+            dual: the Dual frame that will be used to obtain the decomposition weights of an
+                observable when computing its expectation value. For more details, refer to
+                :meth:`get_decomposition_weights`. When this is ``None``, the canonical Dual frame
+                will be constructed from the POVM stored in the ``povm_sample``'s
+                :attr:`.POVMPubResult.metadata`.
 
         Raises:
-            ValueError: If the provided ``dual`` is not a dual frame to the POVM
-                used to produce ``povm_sample``.
+            ValueError: If the provided ``dual`` is not a dual frame to the POVM stored in the
+                ``povm_samples``'s :attr:`.POVMPubResult.metadata`.
         """
         self._povm = povm_sample.metadata.povm_implementation.definition()
-        self.counts: np.ndarray = povm_sample.get_counts()  # type: ignore
-        # TODO: find a way to avoid the type ignore
+
+        self._counts = cast(np.ndarray, povm_sample.get_counts())
 
         if (dual is not None) and (not dual.is_dual_to(self._povm)):
             raise ValueError(
@@ -56,15 +84,21 @@ class POVMPostProcessor:
 
     @property
     def povm(self) -> BasePOVM:
-        """Return the POVM that was used to sample outcomes."""
+        """Return the POVM definition that was used to sample outcomes."""
         return self._povm
 
     @property
+    def counts(self) -> np.ndarray:
+        """Return the histogram of the POVM outcomes via :meth:`.POVMPubResult.get_counts`."""
+        return self._counts
+
+    @property
     def dual(self) -> BaseDual:
-        """Return the dual that is used.
+        """Return the Dual frame that is used.
 
         .. warning::
-            If the dual frame is not already built, this could be computationally demanding.
+            If the dual frame is not already built, accessing this property could be computationally
+            demanding.
         """
         if self._dual is None:
             dual_class = self.povm.default_dual_class
@@ -81,21 +115,24 @@ class POVMPostProcessor:
         self._dual = new_dual
 
     def get_decomposition_weights(
-        self, observable: SparsePauliOp, outcome_set: set[Any]
+        self,
+        observable: SparsePauliOp,
+        outcome_set: set[Any],
     ) -> dict[Any, float]:
-        r"""Get the decomposition weights of ``observable`` into the elements of ``self.povm``.
+        r"""Get the decomposition weights of ``observable`` into the elements of :attr:`povm`.
 
-        Given an observable :math:`O` which is in the span of a given POVM, one
-        can write the observable :math:`O` as the weighted sum of the POVM effects,
-        :math:`O = \sum_k w_k M_k` for real weights :math:`w_k` and where :math:`k`
-        labels the outcomes.
+        Given an observable :math:`O` which is in the span of a POVM (here, :attr:`povm`), one can
+        write :math:`O` as the weighted sum of the POVM effects, :math:`O = \sum_k w_k M_k` for real
+        weights :math:`w_k` and where :math:`k` labels the outcomes.
+
+        See also :meth:`.BaseDual.get_omegas`.
 
         Args:
             observable: the observable to be decomposed into the POVM effects.
-            outcome_set: set of outcome labels indicating which decomposition
-                weights are queried. An outcome of a :class:`.ProductPOVM` is
-                labeled by a tuple of integers for instance. For a :class:`.MultiQubitPOVM`,
-                an outcome is simply labeled by an integer.
+            outcome_set: set of outcome labels indicating which decomposition weights are queried.
+                An outcome of a :class:`.ProductPOVM` is labeled by a tuple of integers for
+                instance. For a :class:`.MultiQubitPOVM`, an outcome is simply labeled by an
+                integer.
 
         Returns:
             A dictionary mapping outcome labels to decomposition weights.
@@ -103,24 +140,24 @@ class POVMPostProcessor:
         return dict(self.dual.get_omegas(observable, outcome_set))  # type: ignore
 
     def get_expectation_value(
-        self, observable: SparsePauliOp, loc: int | tuple[int, ...] | None = None
+        self,
+        observable: SparsePauliOp,
+        loc: int | tuple[int, ...] | None = None,
     ) -> tuple[np.ndarray, np.ndarray] | tuple[float, float]:
-        """Return the expectation value of a given observable and standard deviation of the estimator.
+        """Return the expectation value and standard deviation of the given ``observable``.
 
         Args:
             observable: the observable whose expectation value is queried.
-            loc: this argument is relevant if multiple sets of parameter values
-                were supplied to the sampler in the same :class:`.POVMSamplerPub`.
-                The index ``loc`` then corresponds to the set of parameter values
-                that was supplied to the sampler through the PUB. If None, the
-                expectation value (and standard deviation) for each set of circuit
+            loc: this argument is relevant if multiple sets of parameter values were supplied to the
+                sampler in the same :class:`.POVMSamplerPub`. The index ``loc`` then corresponds to
+                the set of parameter values that was supplied to the sampler through the Pub. If
+                ``None``, the expectation value (and standard deviation) for each set of circuit
                 parameters is returned.
 
         Returns:
-            A tuple of (estimated) expectation value and standard deviation of the
-            estimator if a single value is queried. If all values are queried a
-            tuple of two :class:`numpy.ndarray` is returned, the first containing
-            the expectation values and the second the standard deviations.
+            A tuple of (estimated) expectation value(s) and standard deviation(s). If a single value
+            was queried (via ``loc``), both of these will be a ``float``. Otherwise, they will be
+            instances of :class:`numpy.ndarray`.
         """
         if loc is not None:
             return self._single_exp_value_and_std(observable, loc)
@@ -138,14 +175,13 @@ class POVMPostProcessor:
         observable: SparsePauliOp,
         loc: int | tuple[int, ...],
     ) -> tuple[float, float]:
-        """Return the expectation value of a given observable and standard deviation of the estimator.
+        """Return the expectation value and standard deviation of the given ``observable``.
 
         Args:
             observable: the observable whose expectation value is queried.
-            loc: index of the results to use. The index corresponds to the set
-                of parameter values that was supplied to the sampler through a
-                :class:`.POVMSamplerPub`. If the circuit was not parametrized,
-                the index ``loc`` should be 0.
+            loc: index of the results to use. The index corresponds to the set of parameter values
+                that was supplied to the sampler through a :class:`.POVMSamplerPub`. If the circuit
+                was not parametrized, the index ``loc`` should be 0.
 
         Returns:
             A tuple of (estimated) expectation value and standard deviation.
@@ -168,6 +204,6 @@ class POVMPostProcessor:
         exp_val /= shots
         std /= shots
 
-        std = np.sqrt((std - exp_val**2) / (shots - 1))
+        std = float(np.sqrt((std - exp_val**2) / (shots - 1)))
 
         return exp_val, std
