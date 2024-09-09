fa54306788293cd5afab6e3a16cc662e7ed54ced
Timothée Dao <Timothee.Dao1@ibm.com>
2024-09-09 10:24:15 +0200

    make `combine_counts` a keyword argument

--- povm_toolbox/post_processor/povm_post_processor.py
+++ povm_toolbox/post_processor/povm_post_processor.py
@@ -60,6 +60,7 @@ class POVMPostProcessor:
         self,
         povm_sample: POVMPubResult,
         dual: BaseDual | None = None,
+        *,
         combine_counts: bool = False,
     ) -> None:
         """Initialize the POVM post-processor.
