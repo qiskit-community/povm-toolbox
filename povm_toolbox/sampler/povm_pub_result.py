a1ada24cd36d6563f3f68b49850fc90c97a74641
Max Rossmannek <oss@zurich.ibm.com>
2025-12-02 09:43:50 +0100

    fix: lint after min python update

--- povm_toolbox/sampler/povm_pub_result.py
+++ povm_toolbox/sampler/povm_pub_result.py
@@ -12,17 +12,8 @@
 
 from __future__ import annotations
 
-import sys
 from collections import Counter
-
-if sys.version_info < (3, 10):
-    from typing import Any  # pragma: no cover
-
-    # There is no way to support this type properly in python 3.9, which will be end of life in
-    # November 2025 anyways.
-    EllipsisType = Any  # pragma: no cover
-else:
-    from types import EllipsisType
+from types import EllipsisType
 
 import numpy as np
 from qiskit.primitives.containers import DataBin, PubResult
